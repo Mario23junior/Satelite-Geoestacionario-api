db438849681b88621dd031043098b993d9a532cf
Mario23junior <mariojunior3251@gmail.com>
2021-09-22 21:12:46 -0300

    Iniciando projeto de satelites geoestacionario

---
+++ src/main/java/com/projeto/satellite/GeostationarySatellite/GeostationarySatelliteApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.satellite.GeostationarySatellite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeostationarySatelliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeostationarySatelliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/satellite/GeostationarySatellite/GeostationarySatelliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.satellite.GeostationarySatellite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeostationarySatelliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
