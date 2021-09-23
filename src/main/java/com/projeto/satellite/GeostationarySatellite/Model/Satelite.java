ef97816982e88508d76cd51ff3959ac7b66be2ec
Mario23junior <mariojunior3251@gmail.com>
2021-09-23 05:22:46 -0300

    Criando entidade satelite no modelo de dados

---
+++ src/main/java/com/projeto/satellite/GeostationarySatellite/Model/Satelite.java
@@ -0,0 +1,51 @@
+package com.projeto.satellite.GeostationarySatellite.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Satelite {
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nomeDeDiretriz;
+	private String nomeComun;
+	private String fabricante;
+	private String veiculo;
+
+	public String getNomeDeDiretriz() {
+		return nomeDeDiretriz;
+	}
+
+	public void setNomeDeDiretriz(String nomeDeDiretriz) {
+		this.nomeDeDiretriz = nomeDeDiretriz;
+	}
+
+	public String getNomeComun() {
+		return nomeComun;
+	}
+
+	public void setNomeComun(String nomeComun) {
+		this.nomeComun = nomeComun;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public String getVeiculo() {
+		return veiculo;
+	}
+
+	public void setVeiculo(String veiculo) {
+		this.veiculo = veiculo;
+	}
+
+}
