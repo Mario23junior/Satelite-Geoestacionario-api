d10c2e6d18ff5b3e0b0438f3cf480e3c1fa9f4ea
Mario23junior <mariojunior3251@gmail.com>
2021-09-23 04:39:55 -0300

    Criando entidade de comunicacoes

--- src/main/resources/db/migration/V03__criar_e_inserir_entidade_aeroespacial.sql
+++ src/main/resources/db/migration/V03__criar_e_inserir_entidade_aeroespacial.sql
@@ -6,6 +6,6 @@ CREATE TABLE `Aeroespacial` (
     `lancamento` date(20),
     `operador` varchar(80)
    );
-  
+
 INSERT INTO Aeroespacial (id, peso, vidaUtil, lancamento, operador) VALUES 
 (1,3200,'15 anos', 20111005 ,'Intelsat');
