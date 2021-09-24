196be81054355f286f17c66e7ac2ba3c73009d0a
Mario23junior <mariojunior3251@gmail.com>
2021-09-24 03:37:08 -0300

    Criando modelo da entidade comunicacao

--- src/main/resources/db/migration/V04__criar_e_inserir_entidade_comunicacao.sql
+++ src/main/resources/db/migration/V04__criar_e_inserir_entidade_comunicacao.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `Comunicacao` (
 
     `id` long NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `tipoBanda` varchar(20),
+    `tipo_Banda` varchar(20),
     `ativo` tinyint(12),
     `periodo_Orbital` double(23),
     `Anomalia_Media` double(23)
    );
   
-INSERT INTO Comunicacao (id, tipoBanda, ativo, periodo_Orbital, Anomalia_Media) VALUES 
+INSERT INTO Comunicacao (id, tipo_Banda, ativo, periodo_Orbital, Anomalia_Media) VALUES 
 (1,'Banda C',1, 1436070, 306403);
