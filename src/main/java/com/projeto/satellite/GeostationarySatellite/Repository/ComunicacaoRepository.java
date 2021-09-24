5d07afd1ca32da6edf64ad1aaa66a8fb72b8ca1f
Mario23junior <mariojunior3251@gmail.com>
2021-09-24 04:34:52 -0300

    Criando repository das entidades geolocalizacao e comunicacao

--- src/main/java/com/projeto/satellite/GeostationarySatellite/Repository/ComunicacaoRepository.java
+++ src/main/java/com/projeto/satellite/GeostationarySatellite/Repository/ComunicacaoRepository.java
@@ -3,7 +3,9 @@ package com.projeto.satellite.GeostationarySatellite.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.projeto.satellite.GeostationarySatellite.Model.Comunicacao;
+
 @Repository
-public interface Comunicacao extends JpaRepository<Comunicacao, Long>{
+public interface ComunicacaoRepository extends JpaRepository<Comunicacao, Long>{
 
 }
--- src/main/java/com/projeto/satellite/GeostationarySatellite/Repository/GeolocalizacaoRepository.java
+++ src/main/java/com/projeto/satellite/GeostationarySatellite/Repository/GeolocalizacaoRepository.java
@@ -3,7 +3,9 @@ package com.projeto.satellite.GeostationarySatellite.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.projeto.satellite.GeostationarySatellite.Model.Geolocalizacao;
+
 @Repository
-public interface Geolocalizacao extends JpaRepository<Geolocalizacao, Long>{
+public interface GeolocalizacaoRepository extends JpaRepository<Geolocalizacao, Long>{
 
 }
