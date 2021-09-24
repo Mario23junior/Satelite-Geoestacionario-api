93c736ec1d9e462e49a55d14d87eba0d3af66786
Mario23junior <mariojunior3251@gmail.com>
2021-09-24 03:11:12 -0300

    Criando modelo da entidade geolocalizacao

---
+++ src/main/java/com/projeto/satellite/GeostationarySatellite/Model/Geolocalizacao.java
@@ -0,0 +1,51 @@
+package com.projeto.satellite.GeostationarySatellite.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Geolocalizacao {
+   
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String orbita;
+	private String posicaoEmOrbita;
+	private String localizacaoOrbital;
+	private String cobertura;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getOrbita() {
+		return orbita;
+	}
+	public void setOrbita(String orbita) {
+		this.orbita = orbita;
+	}
+	public String getPosicaoEmOrbita() {
+		return posicaoEmOrbita;
+	}
+	public void setPosicaoEmOrbita(String posicaoEmOrbita) {
+		this.posicaoEmOrbita = posicaoEmOrbita;
+	}
+	public String getLocalizacaoOrbital() {
+		return localizacaoOrbital;
+	}
+	public void setLocalizacaoOrbital(String localizacaoOrbital) {
+		this.localizacaoOrbital = localizacaoOrbital;
+	}
+	public String getCobertura() {
+		return cobertura;
+	}
+	public void setCobertura(String cobertura) {
+		this.cobertura = cobertura;
+	}
+	
+	
+}
