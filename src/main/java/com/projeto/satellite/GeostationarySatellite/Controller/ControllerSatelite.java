da247ff8a1373024ac40a1a95d07bd07009a470c
Mario23junior <mariojunior3251@gmail.com>
2021-09-25 04:53:00 -0300

    Criando controller de salvamento de satelites no banco de dados

--- src/main/java/com/projeto/satellite/GeostationarySatellite/Controller/ControllerSatelite.java
+++ src/main/java/com/projeto/satellite/GeostationarySatellite/Controller/ControllerSatelite.java
@@ -1,29 +1,26 @@
 package com.projeto.satellite.GeostationarySatellite.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.projeto.satellite.GeostationarySatellite.ModelDTO.SateliteDTO;
+import com.projeto.satellite.GeostationarySatellite.Service.ServiceSatelite;
 
 @RestController
 @RequestMapping("/api/v1/project/satelite/")
 public class ControllerSatelite {
-   
+
+	private ServiceSatelite serviceSatelite;
+
+	public ControllerSatelite(ServiceSatelite serviceSatelite) {
+		this.serviceSatelite = serviceSatelite;
+	}
+	
+	@PostMapping
+	public ResponseEntity<SateliteDTO> save(@RequestBody SateliteDTO sateliteDto) {
+		return serviceSatelite.saveSatelite(sateliteDto);
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
