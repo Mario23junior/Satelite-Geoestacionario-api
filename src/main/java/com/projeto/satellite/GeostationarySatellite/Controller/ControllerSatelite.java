e5d339634200bb6da66b927957ecf7dd133af349
Mario23junior <mariojunior3251@gmail.com>
2021-09-27 03:06:00 -0300

    Atualizando informacaoes de dados por id

--- src/main/java/com/projeto/satellite/GeostationarySatellite/Controller/ControllerSatelite.java
+++ src/main/java/com/projeto/satellite/GeostationarySatellite/Controller/ControllerSatelite.java
@@ -6,10 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projeto.satellite.GeostationarySatellite.Model.Satelite;
 import com.projeto.satellite.GeostationarySatellite.ModelDTO.SateliteDTO;
 import com.projeto.satellite.GeostationarySatellite.Service.ServiceSatelite;
 
@@ -41,6 +43,11 @@ public class ControllerSatelite {
 	@GetMapping("nomeDiretriz/{nomeDeDiretriz}")
 	public ResponseEntity<SateliteDTO> listNameDeDiretriz(@PathVariable @Valid String nomeDeDiretriz) {
 		 return serviceSatelite.findByNomeDeDiretriz(nomeDeDiretriz);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<SateliteDTO> updateSatelite(@PathVariable Long id, @RequestBody Satelite satelite) {
+		return serviceSatelite.updateSatelite(id, satelite);
 	}
 	
 	
