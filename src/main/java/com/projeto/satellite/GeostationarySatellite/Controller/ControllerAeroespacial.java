086d70a22ba0235ea7e0e2dd653b84a5d846ef1d
Mario23junior <mariojunior3251@gmail.com>
2021-10-10 23:59:57 -0300

    Criando listagem de informacoes de aeroespacial por id

--- src/main/java/com/projeto/satellite/GeostationarySatellite/Controller/ControllerAeroespacial.java
+++ src/main/java/com/projeto/satellite/GeostationarySatellite/Controller/ControllerAeroespacial.java
@@ -1,6 +1,8 @@
 package com.projeto.satellite.GeostationarySatellite.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +24,10 @@ public class ControllerAeroespacial {
 	@PostMapping
 	public ResponseEntity<AeroespacialDTO> saveAeroespacial(@RequestBody AeroespacialDTO aeroespacialDTO){
 		return aeroespacial.saveAeroespacial(aeroespacialDTO);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<AeroespacialDTO> listAero(@PathVariable Long id) {
+		return aeroespacial.listAreoespacial(id);
 	}
 }
--- src/main/java/com/projeto/satellite/GeostationarySatellite/Service/ServiceAeroespacial.java
+++ src/main/java/com/projeto/satellite/GeostationarySatellite/Service/ServiceAeroespacial.java
@@ -1,10 +1,13 @@
 package com.projeto.satellite.GeostationarySatellite.Service;
 
+import java.util.Optional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.projeto.satellite.GeostationarySatellite.Exceptions.ReturnMessageWhenNoSavedIdFound;
 import com.projeto.satellite.GeostationarySatellite.Model.Aeroespacial;
 import com.projeto.satellite.GeostationarySatellite.ModelDTO.AeroespacialDTO;
 import com.projeto.satellite.GeostationarySatellite.Repository.AeroespacialRepositry;
@@ -29,5 +32,15 @@ public class ServiceAeroespacial {
 	
 	public Aeroespacial saveBody(Aeroespacial aeroespacial) {
 		return aereoRepository.save(aeroespacial);
+	}
+	
+	
+	public ResponseEntity<AeroespacialDTO> listAreoespacial(Long id) {
+		Optional<Aeroespacial> listId = aereoRepository.findById(id);
+		if(listId.isPresent()) {
+			return ResponseEntity.ok(modelMapper.map(listId.get(), AeroespacialDTO.class));
+		} else {
+            throw new ReturnMessageWhenNoSavedIdFound(String.format("Informação não encontrada"));
+		}
 	}
 }
