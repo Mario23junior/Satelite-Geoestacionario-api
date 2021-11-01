9e1a6a2cf4e71810241695b7e13278868289d99a
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2021-10-31 23:56:53 -0300

    Listando informacoes por id

--- src/main/java/com/projeto/satellite/GeostationarySatellite/Controller/ControllerGeomorfologia.java
+++ src/main/java/com/projeto/satellite/GeostationarySatellite/Controller/ControllerGeomorfologia.java
@@ -1,6 +1,8 @@
 package com.projeto.satellite.GeostationarySatellite.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +24,10 @@ public class ControllerGeomorfologia {
 	@PostMapping
 	public ResponseEntity<GeomorfologiaDTO> saveGeomor(@RequestBody GeomorfologiaDTO geomorfologiaDto) {
 		return service.saveGeomor(geomorfologiaDto);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<GeomorfologiaDTO> listIdGeomor(@PathVariable Long id) {
+		return service.listIdGeomorfo(id);
 	}
 }
--- src/main/java/com/projeto/satellite/GeostationarySatellite/Service/ServiceGeomorfologia.java
+++ src/main/java/com/projeto/satellite/GeostationarySatellite/Service/ServiceGeomorfologia.java
@@ -1,10 +1,15 @@
 package com.projeto.satellite.GeostationarySatellite.Service;
 
+import java.util.Optional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.projeto.satellite.GeostationarySatellite.Exceptions.ReturnMessageWhenNoSavedIdFound;
+import com.projeto.satellite.GeostationarySatellite.Exceptions.ValidatingDuplicateValues;
 import com.projeto.satellite.GeostationarySatellite.Model.Geomorfologia;
 import com.projeto.satellite.GeostationarySatellite.ModelDTO.GeomorfologiaDTO;
 import com.projeto.satellite.GeostationarySatellite.Repository.GeomorfologiaRepository;
@@ -28,6 +33,39 @@ public class ServiceGeomorfologia {
 	}
 	
 	public Geomorfologia saveBody (Geomorfologia geomorfologia) {
+		ExceptionDuplicateGeomorfologia(geomorfologia);
 		return geomorRepository.save(geomorfologia);
 	}
+	
+	public void  ExceptionDuplicateGeomorfologia(Geomorfologia geomorfologia) {
+		Geomorfologia findNameAltitudeMinima = geomorRepository.findByAltitudeMinima(geomorfologia.getAltitudeMinima());
+		if(findNameAltitudeMinima != null && findNameAltitudeMinima.getId() != geomorfologia.getId()) {
+			throw new ValidatingDuplicateValues(String.format("Estas informações já foram cadastradas"));
+		}
+	}
+	
+	public ResponseEntity<GeomorfologiaDTO> listIdGeomorfo(@PathVariable Long id) {
+		Optional<Geomorfologia> listId = geomorRepository.findById(id);
+		if(listId.isPresent()) {
+			return ResponseEntity.ok(mapper.map(listId.get(), GeomorfologiaDTO.class));
+		} else {
+ 			throw new ReturnMessageWhenNoSavedIdFound(String.format("Este %s nã foi encontrado no banco de dados",id));
+		}
+	}
 } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
